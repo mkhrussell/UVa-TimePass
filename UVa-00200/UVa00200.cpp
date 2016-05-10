a681c9c09f47dad70f1843861e2bfd62b39e98e6
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2016-05-11 01:36:14 +0600

    Solved and submitted UVa-00200

---
+++ UVa-00200/UVa00200.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <cstring>
+
+char _dict[20000][30];
+
+int _dict_size;
+int _col_size;
+
+char _result[30];
+int _result_cursor = 0;
+void addToResult(char searchKey)
+{
+    bool keyFound = false;
+    for(int i = 0; i < _result_cursor; i++)
+    {
+        if(_result[i] == searchKey)
+        {
+            keyFound = true;
+        }
+    }
+
+    if(!keyFound)
+    {
+        _result[_result_cursor++] = searchKey;
+        _result[_result_cursor] = '\0';
+    }
+}
+
+int main(int argc, char **argv)
+{
+    _dict_size = 0;
+    _col_size = 0;
+    while(fgets(_dict[_dict_size], 30, stdin))
+    {
+        if (strcmp(_dict[_dict_size], "#") == 0 || strcmp(_dict[_dict_size], "#\n") == 0)
+        {
+            break;
+        }
+
+        //printf("%s", _dict[_dict_size]);
+
+        _col_size = strlen(_dict[_dict_size]) > _col_size ? strlen(_dict[_dict_size]) : _col_size;
+        ++_dict_size;
+    }
+
+    //printf("\n\n");
+
+    for(int col = 0; col < _col_size; col++)
+    {
+        for(int row = 0; row < _dict_size; row++)
+        {
+            if(_dict[row][col] >= 'A' && _dict[row][col] <= 'Z')
+            {
+                //printf("%c", _dict[row][col]);
+                addToResult(_dict[row][col]);
+            }
+        }
+        //printf("\n");
+    }
+
+    printf("%s\n", _result);
+
+    return 0;
+}
